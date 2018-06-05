3ab7a6b1d27bb24052ffc9133c3ba3e91230fc7d
Jason Filippou <jason.filippou@gmail.com>
2018-06-05 00:30:39 -0400

    Added an interesting Java problem under demos.

---
+++ src/demos/IntegerTest.java
@@ -0,0 +1,22 @@
+package demos;
+
+/**
+ *  <p>An interesting problem I found online. For you to think about. </p>
+ * @see Integer
+ * @author <a href = "mailto:jason.filippou@gmail.com">Jason Filippou</a>
+ */
+public class IntegerTest {
+
+    public static void main(String[] args){
+
+        /* What will the output of this program be? */
+
+        Integer a = 42;
+        Integer b = 42;
+        System.out.println(a == b);
+        Integer c = 666;
+        Integer d = 666;
+        System.out.println(c == d);
+
+    }
+}
